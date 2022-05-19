aae501e88578598184a99989bc95838793d50da1
bronzo97 <danilobronzo97@hotmail.it>
2022-05-19 18:58:28 +0200

    ...

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 
-
-let typeUsername = document.querySelector(".username").value;
 const typeDistance = document.querySelector(".distance").value;
 
 const generate = document.querySelector(".btn-generate");
 const delate = document.querySelector(".btn-delate");
 
 
-console.log(typeUsername, typeDistance);
-
+let userName = document.querySelector('#name');
 const distance = document.getElementById('');
-const userName = document.getElementById('name');
 
 
 generate.addEventListener("click", function () {
-    userName.innerHTML = `${typeUsername}`;
+    let typeUsername = document.querySelector(".username").value;
+    userName.innerHTML += `${typeUsername}`;
 });
